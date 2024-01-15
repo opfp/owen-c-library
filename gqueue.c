9a310990963e4caa3046fb944f7c284d32c5d0ca
Owen <owp2@pitt.edu>
2024-01-15 16:01:16 -0500

    Fixed buffer overrun in gqueue_as_array

--- gqueue.c
+++ gqueue.c
@@ -1,8 +1,10 @@
 #include "gqueue.h" 
 
 gqueue_obj * gqueue_init() { 
-	gqueue_obj * new_queue = calloc(sizeof(gqueue_obj) + sizeof(gqueue_node),1); 	
-	gqueue_node * new_node = (gqueue_node *) (new_queue+1); 	
+	//gqueue_obj * new_queue = calloc(sizeof(gqueue_obj) + sizeof(gqueue_node),1); 	
+	//gqueue_node * new_node = (gqueue_node *) (new_queue+1); 	
+	gqueue_obj * new_queue = calloc(sizeof(gqueue_obj) ,1); 	
+	gqueue_node * new_node = calloc(sizeof(gqueue_node),1); 	
 	//gqueue_obj new_queue; 
 	//gqueue_node * new_node = calloc(sizeof(gqueue_node) , 1); 
 
@@ -46,7 +48,7 @@ void gqueue_push(gqueue_obj * queue, uint32_t value){
 	assert(!cnode->next);
 
 	//while ( cnode->next ) { cnode = cnode->next; }  
-	if ( cnode->size == (GQUEUE_BLOCK_SZ ) ) { //need to alloc new block  
+	if ( (cnode->s_i + cnode->size) == (GQUEUE_BLOCK_SZ ) ) { //need to alloc new block  
 		gqueue_node * new_b = calloc(sizeof(gqueue_node), 1); 
 		cnode->next = new_b; 	
 		cnode = new_b; 	
@@ -54,13 +56,18 @@ void gqueue_push(gqueue_obj * queue, uint32_t value){
 		queue->node_ct++; 
 	} 
 
+	assert( ( cnode->s_i + cnode->size ) < GQUEUE_BLOCK_SZ ); 
 	cnode->elems[cnode->s_i + cnode->size] = value;  
 	cnode->size++; 
 }   
 
 uint32_t * gqueue_as_array( gqueue_obj * queue, unsigned * ct_wb ) { 
-	size_t arr_sz = ( ( queue->node_ct - 1 ) * GQUEUE_BLOCK_SZ ) + 
-		( queue->tail->size * GQUEUE_BLOCK_SZ ) ;  
+	unsigned elem_ct_u = ( ( queue->node_ct - 1 ) * GQUEUE_BLOCK_SZ ) + queue->tail->size ;  
+	if ( !elem_ct_u ) { 
+		*ct_wb=0; 
+		return NULL; 
+	} 
+	size_t arr_sz = elem_ct_u * sizeof(uint32_t); 
 
 	//gqueue_node * c_node = queue->head; 		
 	//while ( c_node )  { 
@@ -68,16 +75,20 @@ uint32_t * gqueue_as_array( gqueue_obj * queue, unsigned * ct_wb ) {
 	//} 
 
  	//	size_t arr_sz = queue->node_ct * sizeof(gqueue_node) * sizeof(uint32_t);  
-	uint32_t * arr = malloc(arr_sz );  
+	//uint32_t * arr = calloc(arr_sz,1 );  
+	uint32_t * arr = malloc(arr_sz); 	
 	// traverse queue 
 	uint32_t * dest = arr; 
 	gqueue_node * c_node = queue->head; 
-	while (c_node) { 
+	while (c_node && elem_ct_u--) { 
 		// copy  from s_i to s_i + size  
 		memcpy(dest, c_node->elems+c_node->s_i, c_node->size * sizeof(uint32_t) ); 
 		dest += c_node->size; 
 		c_node=c_node->next; 
 	} 
+
+	assert ( !c_node && "Elem undercount"); 
+
 	unsigned elem_ct = dest - arr; 
 	//printf("elem_
 	//assert(elem_ct <= arr_ct); 
